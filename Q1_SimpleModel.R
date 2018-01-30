618aca3ea82c15d27a955fad2ee9a5f2d156aabc
Manuel Rosales <manny@manuels-mbp.attlocal.net>
2018-01-29 22:41:10 -0600

    Comparing Models

--- Q1_SimpleModel.R
+++ Q1_SimpleModel.R
@@ -2,14 +2,15 @@
 # https://github.com/susanli2016/Data-Analysis-with-R/blob/master/Predict-House-Price.Rmd 
 
 # Load necessary packages
-load.lib <- c("olsrr","ggplot2")
+load.lib <- c("olsrr","ggplot2","GGally","caret")
 
 install.lib <- load.lib[!load.lib %in% installed.packages()]
-for(lib in install.lib) install.packages(lib,dependences=TRUE)
+for(lib in install.lib){
+  install.packages(lib,dependences=TRUE)
+} 
 sapply(load.lib,require,character=TRUE)
 
 # Set WD, should be the location of your cloned repository
-
 data.Train = read.csv("train.csv")
 
 # Determine which columns contain most nulls (null count divided by total row count)
@@ -21,81 +22,76 @@ nullFrame
 
 # Now that we know which columns have multiple Null values, we can omit them from our list
 
-# We have been tasked with providing a simple model to help realty agents and prospective
-# buyers determine a potential sales price of a home given certain variables.
-# This is intended to be easy to measure and interpret, therefore we will use what we believe
-# to be the most important variables concerning a home in general: Gross Living Area,
-# Overall Condition, and Overall Quality.
-
 # Review simple model of interest
 simpleModel = lm(data = data.Train, SalePrice ~ GrLivArea + OverallCond + OverallQual)
 par(mfrow=c(2,2)); plot(simpleModel); par(mfrow=c(1,1)); # Generate base R Residual plot on model (need transform on Y)
-olsrr::ols_dsrvsp_plot(simpleModel) # Residual plot
-olsrr::ols_rsd_hist(simpleModel)    # Hitogram of residuals with normal curve
-olsrr::ols_rsd_qqplot(simpleModel)  # Normal QQ Plot
-olsrr::ols_cooksd_barplot(simpleModel) # Cooks D Plot
-olsrr::ols_rsdlev_plot(simpleModel) # Leverage Plot 
+bestSub <- olsrr::ols_best_subset(simpleModel) # Find best model given available explanatory variables
+bestSub; plot(bestSub) # Display results and plot
 
-# Review combinations of GrLivArea, OverallCond, and OverallQual to find best on key measures
-bestSub <- olsrr::ols_best_subset(simpleModel)
-bestSub
-plot(bestSub)
-# Resulting model is SalesPrice ~ GrLivArea + OverallQual, OverallCond did not aid the model
-# based on R2 remaining the same, Adj. R2 going down and C(p)/AIC/SBIC/SBC increasing
-simpleModel = lm(data = data.Train, SalePrice ~ GrLivArea + OverallQual)
-par(mfrow=c(2,2)); plot(simpleModel); par(mfrow=c(1,1)); # Generate base R Residual plot on model (need transform on Y)
-olsrr::ols_dsrvsp_plot(simpleModel) # Residual plot
-olsrr::ols_rsd_hist(simpleModel)    # Hitogram of residuals with normal curve
-olsrr::ols_rsd_qqplot(simpleModel)  # Normal QQ Plot
-olsrr::ols_cooksd_barplot(simpleModel) # Cooks D Plot
-olsrr::ols_rsdlev_plot(simpleModel) # Leverage Plot 
+##### Cross Validation #####
+model1 = caret::train(SalePrice ~ GrLivArea + OverallQual, data = data.Train, method = "lm",
+                      trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                      na.action = na.omit)
+sum(residuals(model1$finalModel)^2, na.rm=T) # CV Press = 2.63e+12
 
-
-# Create transformed response variable log(SalePrice) and explanatory variable log(GrLivArea)
+# Create transformed response variable log(SalePrice) to account for Residuals vs Fitted trend
 data.Train$SalePrice = log(data.Train$SalePrice)
+
+simpleModel = lm(data = data.Train, SalePrice ~ GrLivArea + OverallCond + OverallQual)
+par(mfrow=c(2,2)); plot(simpleModel); par(mfrow=c(1,1)); # Observe better randomized Residuals vs Fitted
+bestSub <- olsrr::ols_best_subset(simpleModel) # Find best model given available explanatory variables
+bestSub; plot(bestSub) # Display results and plot
+
+##### Cross Validation #####
+model2 = caret::train(SalePrice ~ GrLivArea + OverallCond + OverallQual, data = data.Train, method = "lm",
+                      trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                      na.action = na.omit)
+sum(residuals(model2$finalModel)^2, na.rm=T) # CV Press = 60.02
+
+# Create transformed explanatory variable log(GrLivArea) to account for Residuals vs Fitted trend
 data.Train$GrLivArea = log(data.Train$GrLivArea)
 
-logModel = lm(data = data.Train, SalePrice ~ GrLivArea + OverallQual)
+simpleModel = lm(data = data.Train, SalePrice ~ GrLivArea + OverallCond + OverallQual)
+par(mfrow=c(2,2)); plot(simpleModel); par(mfrow=c(1,1)); # Observe better randomized Residuals vs Fitted
+bestSub <- olsrr::ols_best_subset(simpleModel) # Find best model given available explanatory variables
+bestSub; plot(bestSub) # Display results and plot
 
-par(mfrow=c(2,2)); plot(logModel); par(mfrow=c(1,1));
-olsrr::ols_dsrvsp_plot(logModel)    # Residual plot (Id = 524 and Id = 1299 may warrant removal)
-olsrr::ols_rsd_hist(logModel)       # Hitogram of residuals with normal curve
-olsrr::ols_rsd_qqplot(logModel)     # Normal QQ Plot
-olsrr::ols_cooksd_barplot(logModel) # Cooks D Plot (Id 1299 has highest Cook's, 524 second highest)
-olsrr::ols_rsdlev_plot(logModel)    # Leverage Plot
+##### Cross Validation #####
+model3 = caret::train(SalePrice ~ GrLivArea + OverallCond + OverallQual, data = data.Train, method = "lm",
+                      trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                      na.action = na.omit)
+sum(residuals(model3$finalModel)^2, na.rm=T) # CV Press = 60.02
 
-# Remove two worst points in data set for model
-data.Train2 = data.Train[data.Train$Id!=1299 & data.Train$Id!=524, ]
+# Resulting model is log(SalePrice) ~ log(GrLivArea) + OverallQual, OverallCond 
+olsrr::ols_dsrvsp_plot(simpleModel)    # Residual plot 
+olsrr::ols_cooksd_barplot(simpleModel) # Cook's D Bar Plot
 
-logModel2 = lm(data = data.Train2, SalePrice ~ GrLivArea + OverallQual)
+# Remove worst points in data set for model
+worstOutliers <- c(1299,524,31,496,968)
+data.Train = data.Train[!(data.Train$Id %in% worstOutliers), ]
 
-par(mfrow=c(2,2)); plot(logModel2); par(mfrow=c(1,1));
-olsrr::ols_dsrvsp_plot(logModel2)    # Residual plot 
-olsrr::ols_rsd_hist(logModel2)       # Hitogram of residuals with normal curve
-olsrr::ols_rsd_qqplot(logModel2)     # Normal QQ Plot
-olsrr::ols_cooksd_barplot(logModel2) # Cooks D Plot (Id 1299 has highest Cook's, 524 second highest)
-olsrr::ols_rsdlev_plot(logModel2)    # Leverage Plot
+##### Cross Validation #####
+model4 = caret::train(SalePrice ~ GrLivArea + OverallCond + OverallQual, data = data.Train, method = "lm",
+                              trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                              na.action = na.omit)
+sum(residuals(model4$finalModel)^2, na.rm=T) # CV Press = 52.42
 
-VIF <- olsrr::ols_vif_tol(logModel2)  # Determine if VIF is appropriate
+corCols <- data.Train[,c("SalePrice","GrLivArea","OverallCond","OverallQual")]
+GGally::ggpairs(corCols)
+
+olsrr::ols_dsrvsp_plot(simpleModel)    # Residual plot 
+olsrr::ols_rsd_hist(simpleModel)       # Hitogram of residuals with normal curve
+olsrr::ols_rsd_qqplot(simpleModel)     # Normal QQ Plot
+olsrr::ols_cooksd_barplot(simpleModel) # Cooks D Plot (Id 1299 has highest Cook's, 524 second highest)
+olsrr::ols_rsdlev_plot(simpleModel)    # Leverage Plot
+
+VIF <- olsrr::ols_vif_tol(simpleModel)  # Determine if VIF is appropriate
 VIF # VIF for model is low
 
-# Generate results of logModel2
-summary(logModel2) #R2 = .7642, Adj R2 = .7639 
-confint(logModel2)
-
-# Interpretation here is 
-# log(SalePrice) = 7.66 + .46*log(GrLivArea) + .17*OverallQual
-# 2^.46 = 1.37, so for every doubling of GrLivArea there is a 1.37 increase in median sale price accounting for OverallQual
-# 95% CI [1.336196, 1.408642]
-# Need to see how to interpret OverallQual here...
-# CI needed
-
-
-
-
-
-
-
+ggplot(data.Train,aes(y=SalePrice,x=GrLivArea)) + geom_point(color="blue") + 
+  labs(title = "log(Sales Price) vs log(Living Area)", y="log of Sales Price ($)", x="log of Gross Living Area") +
+  theme(axis.title=element_text(size=14,face="bold"), title=element_text(size=14,face="bold"), 
+        axis.text = element_text(size=10), legend.position = "bottom")
 
 ############################ OLD STUFF AFTER HERE ###################################
 
@@ -106,26 +102,6 @@ confint(logModel2)
 #   data.Train[,cols] = NULL
 # }
 # ncol(data.Train)
-# 
-# train$Alley = NULL; test$Alley = NULL
-# train$PoolQC = NULL; test$PoolQC = NULL
-# train$Fence = NULL; test$Fence = NULL
-# train$FireplaceQu = NULL; test$FireplaceQu = NULL
-# train$OpenPorchSF = NULL ; test$OpenPorchSF = NULL 
-# train$WoodDeckSF = NULL; test$WoodDeckSF = NULL
-# train$Street = NULL; test$Street = NULL
-# train$LandContour = NULL; test$LandContour = NULL
-# train$LandSlope = NULL; test$LandSlope = NULL
-# train$Condition2 = NULL; test$Condition2 = NULL
-# train$RoofMatl = NULL; test$RoofMatl = NULL
-# train$BsmtCond = NULL; test$BsmtCond = NULL
-# train$BsmtFinType2 = NULL; test$BsmtFinType2 = NULL
-# train$Heating = NULL; test$Heating = NULL
-# train$Functional = NULL; test$Functional = NULL
-# train$GarageQual = NULL; test$GarageQual = NULL
-# train$GarageCond = NULL; test$GarageCond = NULL
-# train$MiscFeature  = NULL; test$MiscFeature  = NULL
-# train$Utilities = NULL; test$Utilities = NULL
 # 
 # ##### Figure 1 #####
 # # Scatter Plot by Neighborhood (raw data) 
@@ -152,36 +128,8 @@ confint(logModel2)
 # head(Cooks.df,10) # Top 10 leverage points
 # 
 # plot(CooksInitial, ylab = "Cooks Distance")
-# 
-# # Pearson's R = 0.7156116, Adjust R^2 = .5056
-# model_int = lm(log_SalePrice ~ log_GrLivArea + 
-#                  Neighborhood + log_GrLivArea:Neighborhood, data = data.Train) 
-# 
-# # Assumptions for Residuals not met, visible in residual plots and histogram
-# ##### Figure 3 #####
-# par(mfrow=c(2,2)); plot(initial_model)
-# par(mfrow=c(1,1)); ols_rsd_hist(initial_model)
-# 
-# # Remove 2 data points with highest cooks distance (Id = 1299 or 524)
-# # These two points are also the highest cooks distance in the MLR containing interaction.
-# data.Train = data.Train[data.Train$Id!=1299 & data.Train$Id!=524,  ]
-# 
-# # Fit Initial Model without high Cook's rows
-# ##### Table 1 #####
-# initial_model = lm(log_SalePrice~log_GrLivArea, data=data.Train)
-# summary(initial_model)
-# 
-# # Generate MLR with interaction
-# ##### Table 2 #####
-# model_int = lm(log_SalePrice ~ log_GrLivArea + Neighborhood + log_GrLivArea:Neighborhood, data = data.Train) #R2 = .5216, high VIF
-# summary(model_int)
-# 
-# ##### Figure 4 #####
-# par(mfrow=c(2,2)); plot(model_int)
-# par(mfrow=c(1,1)); ols_rsd_hist(model_int)
-# 
-# # Plot with interaction and 95% CI
-# ##### Figure 5 #####
+
+
 # ggplot(data.Train,aes(y=log_SalePrice,x=log_GrLivArea,colour=Neighborhood))+geom_point()+geom_smooth(method = "lm") +
 #   labs(title = "log(Sales Price) vs log(Living Area)", y="log of Sales Price ($)", x="log of Living Area (100 sq ft)") +
 #   theme(axis.title=element_text(size=14,face="bold"), title=element_text(size=14,face="bold"), 
@@ -201,15 +149,4 @@ confint(logModel2)
 # #abline(a = 11.74212 + .11, b = .81965-.34662, col="green")
 # #abline(a = 11.74212 - .03, b = .81965-.14631, col="red")
 # #abline(a = 11.74, b = .81965, col = "blue")
-# # 
-# # # Group Edwards and BrkSide into 1 category for Neighborhood
-# # grouped = data.Train
-# # grouped$Neighborhood2 = ifelse(grouped$Neighborhood == "NAmes", "NAmes","BrkSide/Edwards")
-# # model2group = lm(log_SalePrice ~ log_GrLivArea + Neighborhood2 + log_GrLivArea:Neighborhood2, data = grouped)
-# # summary(model2group) #Pearson's R = 0.7242 and R2 = .5206
-# # vif(model2group)
-# # ##### Table 5 #####
-# # confint(model_int)
-# # # Compare full to reduced model, see if the extra terms are statistically significant
-# # ##### Table 3 #####
-# # Anova(model_int, model2group)
+
