5023e02a272cc77d978a2cf8278c3104b6f5ea26
Manuel Rosales <manny@Manuels-MacBook-Pro.local>
2018-01-30 17:53:18 -0600

    Updates

---
+++ Q1_ModelSummaryStats.R
@@ -0,0 +1,101 @@
+# https://rpubs.com/Ludovicbenistant/HousePrice
+# https://github.com/susanli2016/Data-Analysis-with-R/blob/master/Predict-House-Price.Rmd 
+
+# Load necessary packages
+load.lib = c("olsrr","ggplot2","caret")
+
+install.lib = load.lib[!load.lib %in% installed.packages()]
+for(lib in install.lib){
+  install.packages(lib,dependences=TRUE)
+} 
+
+sapply(load.lib,require,character=TRUE)
+
+# Function to generate summary dataframes for different models
+# x = vector of names to append in a lm() statement
+# y = initialized data frame to append results for each model
+GenerateFitStats = function(x,y){
+  for(i in 1:length(x)){
+    my.formula = paste("SalePrice ~ GrLivArea +",x[i], sep=" ")
+    my.model = lm(data = model.df, my.formula)
+    R2 = summary(lm(data = model.df, my.formula))$r.squared
+    AdjR2 = summary(lm(data = model.df, my.formula))$adj.r.squared
+    AIC = ols_aic(my.model); SBC = ols_sbc(my.model);
+    modelstats.df = data.frame(Model=my.formula, R2=R2, AdjR2=AdjR2, AIC=AIC, SBC=SBC) 
+    y = rbind(y,modelstats.df)
+  }
+  return(y) 
+}
+
+# Set WD, should be the location of your cloned repository
+data.Train = read.csv("train.csv")
+
+# Determine Categorical and Continuous Variables
+CatColumns = data.Train[,sapply(data.Train,is.factor)]
+CatColNames = names(CatColumns)
+
+# Set up data set of Id, GrLivArea, SalePrice to create model statistics for each categorical variable
+model.df = data.Train[,c("Id","GrLivArea","SalePrice")]
+model.df = cbind(model.df,CatColumns)
+
+rawSummary = data.frame(Model=as.character(), R2=as.numeric(), AdjR2=numeric(), AIC=numeric(), SBC=numeric()) 
+rawSummary.results = GenerateFitStats(CatColNames,rawSummary) # Call Function For Fit Stats
+
+# Take log of Response to determine fit statistics
+model.df$SalePrice = log(model.df$SalePrice)
+logxSummary = data.frame(Model=as.character(), R2=as.numeric(), AdjR2=numeric(), AIC=numeric(), SBC=numeric()) 
+logxSummary.results = GenerateFitStats(CatColNames,logxSummary) # Call Function For Fit Stats
+
+# Take log of explanatory continuous varible to determine fit statistics
+model.df$GrLivArea = log(model.df$GrLivArea)
+logxlogySummary= data.frame(Model=as.character(), R2=as.numeric(), AdjR2=numeric(), AIC=numeric(), SBC=numeric()) 
+logxlogySummary.results = GenerateFitStats(CatColNames,logxlogySummary) # Call Function For Fit Stats
+
+# Review Best Raw Data Model
+BestRawModel = lm(data = data.Train, SalePrice ~ GrLivArea + Neighborhood)
+par(mfrow=c(2,2)); plot(BestRawModel); par(mfrow=c(1,1)); # Generate base R Residual plot on model (need transform)
+
+# Review Best log(Y) Model
+BestlogYModel = lm(data = data.Train, log(SalePrice) ~ GrLivArea + Neighborhood)
+par(mfrow=c(2,2)); plot(BestlogYModel); par(mfrow=c(1,1)); # Generate base R Residual plot on model
+
+# Review Best log(Y) and log(X) Model
+BestlogYlogXModel = lm(data = data.Train, log(SalePrice) ~ log(GrLivArea) + Neighborhood)
+par(mfrow=c(2,2)); plot(BestlogYlogXModel); par(mfrow=c(1,1)); # Generate base R Residual plot on model
+
+# Generate pretty Residual Plots
+olsrr::ols_dsrvsp_plot(BestRawModel)
+olsrr::ols_dsrvsp_plot(BestlogYModel)   
+olsrr::ols_dsrvsp_plot(BestlogYlogXModel)   
+
+# CV Press Statistic on 10 cross validations
+cvmodel1 = caret::train(SalePrice ~ GrLivArea + Neighborhood, data = data.Train, method = "lm",
+                       trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                       na.action = na.omit)
+sum(residuals(cvmodel1$finalModel)^2, na.rm=T) # CV Press = 2.407736e+12
+
+cvmodel2 = caret::train(log(SalePrice) ~ GrLivArea + Neighborhood, data = data.Train, method = "lm",
+                       trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                       na.action = na.omit)
+sum(residuals(cvmodel2$finalModel)^2, na.rm=T) # CV Press = 56.1487
+
+cvmodel3 = caret::train(log(SalePrice) ~ log(GrLivArea) + Neighborhood, data = data.Train, method = "lm",
+                       trControl = trainControl(method = "cv", number = 10,verboseIter = TRUE),
+                       na.action = na.omit)
+sum(residuals(cvmodel3$finalModel)^2, na.rm=T) # CV Press = 53.69073
+
+VIF = olsrr::ols_vif_tol(BestlogYlogXModel)  # Determine if VIF is appropriate
+VIF # VIF for model is low
+ 
+ScatterPlot = ggplot(data.Train,aes(y=log(SalePrice),x=log(GrLivArea),color=Neighborhood)) + geom_point() +
+   labs(title = "log(Sales Price) vs log(Living Area)", y="log of Sales Price ($)", x="log of Gross Living Area") +
+   theme(axis.title=element_text(size=14,face="bold"), title=element_text(size=14,face="bold"), 
+         axis.text = element_text(size=10), legend.position = "bottom") 
+
+ScatterByFactor = ggplot(data.Train,aes(y=log(SalePrice),x=log(GrLivArea))) + geom_point() + geom_smooth(method = lm,se = TRUE) +
+  labs(title = "log(Sales Price) vs log(Living Area)", y="log of Sales Price ($)", x="log of Gross Living Area") +
+  theme(axis.title=element_text(size=14,face="bold"), title=element_text(size=14,face="bold"), 
+        axis.text = element_text(size=10), legend.position = "bottom") + facet_wrap( ~ Neighborhood, ncol=5)
+  
+ScatterPlot 
+ScatterByFactor
